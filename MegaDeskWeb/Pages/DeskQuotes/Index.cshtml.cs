d2e7d02afc31a6995d62afc8abd2d5484100c077
Justen N <losthalflingrogue@gmail.com>
2019-06-18 11:21:08 -0600

    sortable name and date

--- MegaDeskWeb/Pages/DeskQuotes/Index.cshtml.cs
+++ MegaDeskWeb/Pages/DeskQuotes/Index.cshtml.cs
@@ -12,6 +12,8 @@ namespace MegaDeskWeb.Pages.DeskQuotes
 {
     public class IndexModel : PageModel
     {
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
         private readonly MegaDeskWeb.Models.MegaDeskWebContext _context;
 
         public IndexModel(MegaDeskWeb.Models.MegaDeskWebContext context)
@@ -21,9 +23,35 @@ namespace MegaDeskWeb.Pages.DeskQuotes
 
         public IList<DeskQuote> DeskQuote { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
-            DeskQuote = await _context.DeskQuote
+        
+            NameSort = String.IsNullOrEmpty(sortOrder)? "name_desc": "";
+            DateSort = sortOrder == "Date" ? "date_desc":"date";
+
+            IQueryable<DeskQuote> deskQuoteIQ = from d in _context.DeskQuote
+                                                select d;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                deskQuoteIQ = deskQuoteIQ.OrderByDescending(d => d.Name);
+                break;
+
+                case "Date" : 
+                deskQuoteIQ = deskQuoteIQ.OrderBy(d => d.QuoteDate);
+                break;
+
+                case "date_desc":
+                deskQuoteIQ = deskQuoteIQ.OrderByDescending(d => d.QuoteDate);
+                break;
+
+                default:
+                deskQuoteIQ = deskQuoteIQ.OrderBy(d => d.Name);
+                break;
+            }
+            
+            DeskQuote = await deskQuoteIQ
                 .Include(d => d.Desk)
                 .Include(d => d.RushShipping).ToListAsync();
         }
